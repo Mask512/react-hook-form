ba3ebed7a4fe381d7a447fc057b1f00c43b9d836
Mask512 <Mask.roktz@gmail.com>
2024-04-10 03:26:36 +0700

    instal react hook form & managing state

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,15 +1,29 @@
+import { useForm } from 'react-hook-form';
 import './styles.css';
 
 export const YoutubeForm = () => {
+  const form = useForm();
+  const { register } = form;
+
+  // const { name, ref, onChange, onBlur } = register('username');
+
   return (
     <div className="youtube-form">
       <form>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
+        <input
+          type="text"
+          id="username"
+          // name={name}
+          // ref={ref}
+          // onChange={onChange}
+          // onBlur={onBlur}
+          {...register('username')}
+        />
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" />
+        <input type="email" id="email" {...register('email')} />
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
+        <input type="text" id="channel" {...register('username')} />
         <button>Submit</button>
       </form>
     </div>
